1ae4fb99825490534208ceb75c70ee32c156f230
vovan46rus <punk.potap@gmail.com>
2023-09-05 13:14:13 +0300

    new domen

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.250.101.69', '127.0.0.1', 'localhost', 'ttaski.ddns.net']
 
 
 # Application definition
